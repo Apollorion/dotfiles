e57e401b2d860b38621ab2fc5552c9606e807e5b
apollorion <joey@apollorion.com>
2022-04-30 00:22:52 -0400

    big work

---
+++ .bootstrap/main.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Install brew stuff
+cd ~
+brew bundle install
+cd -
+
+# Install OHMYZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Import public pgp key
+gpg --import ~/keys/gpg-pub--2022-04-29.asc
+
+# Set default applications for file types
+duti -s com.sublimetext.4 .md all
+duti -s com.sublimetext.4 .txt all
+duti -s com.sublimetext.4 .yaml all
+duti -s com.sublimetext.4 .yml all
+
+
+# Install / configure asdf plugins
+asdf plugin add awscli
+asdf plugin add golang
+asdf plugin add kubectl
+asdf plugin add terraform
+asdf plugin add nodejs
+asdf plugin add yarn
+
+asdf install awscli latest
+asdf install golang latest
+asdf install kubectl latest
+asdf install terraform latest
+asdf install nodejs latest
+asdf install yarn latest
+
+# NPM
+npm install --global expo-cli
+
+# OSX defaults
+defaults write com.apple.dock tilesize -float 36 # Sets dock size
+defaults write com.apple.dock show-recents -bool false # Disable recents in the dock
+defaults com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -integer 2 # Right click with bottom right corner of trackpad
+defaults -g com.apple.swipescrolldirection -bool false # Disable natural scroll direction
+defaults -g com.apple.mouse.scaling -float 20.0 # Speed up the mouse
---
+++ README.md
@@ -0,0 +1,11 @@
+# Joeys Dotfiles
+
+## Installation
+
+1. Install Homebrew - brew.sh
+2. Install Yadm `brew install yadm`
+3. Install brewfile `brew install rcmdnk/file/brew-file`
+4. Using yadm, clone this repo `yadm clone https://github.com/Apollorion/dotfiles.git`
+5. Restart shell
+6. If not asked, run bootstrap `yadm bootstrap`
+
