77147ce818b07d56753385fe6db3d036760325c9
apollorion <joey@apollorion.com>
2022-05-02 09:34:43 -0400

    updates to secrets template

--- .bootstrap/main.sh
+++ .bootstrap/main.sh
@@ -8,15 +8,11 @@ cd -
 # Install OHMYZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Import public pgp key
-gpg --import ~/keys/gpg-pub--2022-04-29.asc
-
 # Set default applications for file types
 duti -s com.sublimetext.4 .md all
 duti -s com.sublimetext.4 .txt all
 duti -s com.sublimetext.4 .yaml all
 duti -s com.sublimetext.4 .yml all
-
 
 # Install / configure asdf plugins
 asdf plugin add awscli
@@ -35,6 +31,11 @@ asdf install yarn latest
 
 # NPM
 npm install --global expo-cli
+
+# Copy secrets template if none exist
+if [ ! -f ~/secrets.sh ]; then
+    cp ~/secret_template.sh ~/secrets.sh
+fi
 
 # OSX defaults
 defaults -currentHost write com.apple.dock tilesize -float 36 # Sets dock size
---
+++ secret_template.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# github token
+export GH_TOKEN=""
+
+# dockerhub
+export DOCKERHUB_USERNAME=""
+export DOCKERHUB_PASSWSORD=""
